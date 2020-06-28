b586f306c5c350430db1ff9cca9738119bc4b720
CodeSingh <sugi007@yahoo.com>
2020-06-28 14:23:05 +0100

    Created flask website
    Created flask website to configure scoreboard settings

---
+++ web/constants.py
@@ -0,0 +1,7 @@
+DICT_TEAMS = {1: 'Arsenal', 2: 'Aston Villa', 3: 'Bournemouth', 
+4:'Brighton & Hove Albion', 5: 'Burnley', 6: 'Chelsea', 
+7: 'Crystal Palace', 8: 'Everton', 9: 'Leicester City', 
+10: 'Liverpool', 11: 'Manchester City', 12: 'Manchester United', 
+13: 'Newcastle United', 14: 'Norwich City', 15: 'Sheffield United', 
+16: 'Southampton', 17: 'Tottenham Hotspur', 18: 'Watford', 
+19: 'West Ham United', 20: 'Wolverhampton Wanderers'}
---
+++ web/website.py
@@ -0,0 +1,46 @@
+'''
+Code created by Matt Richardson 
+for details, visit:  http://mattrichardson.com/Raspberry-Pi-Flask/inde...
+'''
+from flask import Flask, render_template, request, redirect, url_for
+import datetime
+import json
+import os
+from config_update import get_config, set_config
+import constants as c
+import time
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+   model_index = { "teams" : c.DICT_TEAMS, "data" : get_config() }
+
+   print(model_index)
+   return render_template('index.html', **model_index)
+
+@app.route('/reboot',methods = ['POST'])
+def reboot():
+   if request.method == 'POST':
+      os.system('sudo reboot')
+      time.sleep(10)
+      return redirect(url_for('index'))
+   #else:
+   #   user = request.args.get('nm')
+   #   return redirect(url_for('success',name = user))
+
+@app.route('/save',methods = ['POST'])
+def save():
+   if request.method == 'POST':
+      config = get_config()
+      config['state'] = request.form['state']
+      config['team'] = request.form['team']
+      config['weather_location'] = request.form['weather_location']
+      set_config(config)
+
+   return redirect(url_for('index'))
+   #else:
+   #   user = request.args.get('nm')
+   #   return redirect(url_for('success',name = user))
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
