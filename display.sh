c56f01a3ca1f979405b56495515d9d2fcf9e5ad3
Sukhjit Singh <sugi007@yahoo.com>
2020-10-13 20:38:28 +0100

    New led matrix type requires different initialisation

--- display.sh
+++ display.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/python3 /home/pi/rpi-led-scoreboard/image_viewer.py --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-brightness=40 &
+/usr/bin/python3 /home/pi/rpi-led-scoreboard/image_viewer.py --led-cols=64 --led-gpio-mapping=adafruit-hat-pwm --led-brightness=40 --led-panel-type=FM6126A &
