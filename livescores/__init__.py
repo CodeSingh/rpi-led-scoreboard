ce1ed2bb7fb1ecf7db8ba6b7e9ff998fd0a53a26
CodeSingh <sugi007@yahoo.com>
2022-11-20 07:38:35 +0000

    Added world cup to __init__.py

--- livescores/__init__.py
+++ livescores/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['livescore', 'bbc_fac_livescore', 'bbc_epl_livescore', 'bbc_mls_livescore', 'bbc_laliga_livescore']
+__all__ = ['livescore', 'bbc_fac_livescore', 'bbc_epl_livescore', 'bbc_mls_livescore', 'bbc_laliga_livescore', 'bbc_worldcup_livescore']
