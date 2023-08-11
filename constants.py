ce808e648393fa38af0c59ac4502ddd17b49c724
CodeSingh <sugi007@yahoo.com>
2023-08-11 10:46:20 +0100

    Added new teams to constants file

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
-DICT_TEAMS = {1: 'Arsenal', 2: 'Aston Villa', 3: 'Fulham', 
-4:'Brighton & Hove Albion', 5: 'Burnley', 6: 'Chelsea', 
-7: 'Crystal Palace', 8: 'Everton', 9: 'Leicester City', 
-10: 'Liverpool', 11: 'Manchester City', 12: 'Manchester United', 
-13: 'Newcastle United', 14: 'Leeds United', 15: 'Sheffield United', 
-16: 'Southampton', 17: 'Tottenham Hotspur', 18: 'West Bromwich Albion', 
+DICT_TEAMS = {1: 'Arsenal', 2: 'Aston Villa', 3: 'AFC Bournemouth', 
+4:'Brentford', 5: 'Brighton & Hove Albion', 6: 'Burnley', 
+7: 'Chelsea', 8: 'Crystal Palace', 9: 'Everton', 
+10: 'Fulham', 11: 'Liverpool', 12: 'Luton Town', 
+13: 'Manchester City', 14: 'Manchester United', 15: 'Newcastle United', 
+16: 'Nottingham Forest', 17: 'Sheffield United', 18: 'Tottenham Hotspur', 
 19: 'West Ham United', 20: 'Wolverhampton Wanderers'}
 DICT_TEMP_TYPES = {"metric": "'C", "imperial": "'F" }
 BASE_PATH_SCOREBOARD = '/home/pi/rpi-led-scoreboard/'
